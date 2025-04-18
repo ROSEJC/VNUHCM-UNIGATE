ca6c2f45fa9150a947b654f885372113135d4113
jnswkz <23520408@gm.uit.edu.vn>
2025-04-18 14:52:39 +0700

    Update dependencies and refactor App component structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 import Forum from './Forum.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
-    <Forum />
-
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+);
